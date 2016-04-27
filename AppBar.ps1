abab36b486596475e12047439a5a0d0c64bba266
Clijsters <dominique.clijsters@gmail.com>
2016-04-27 14:51:10 +0200

    Removed some Declares, moved initialization from Assembly to PowerShell.
    removed enumwindows
    removed isWindowVisible
    removed EnumWindowProc
    removed GetWindowText
    Moved Declaration and Initialization of TimeLabel, Panel, and
    StartButton to PowerShell.

--- AppBar.ps1
+++ AppBar.ps1
@@ -9,12 +9,48 @@ cd $scriptPath
 #TODO2:Put Region Designer to PowerShell.
 AddAssembly "AppBar"
 
-$Bar = New-Object AppBar ABE_TOP
+[Shell4Core.AppBar]$Bar = New-Object Shell4Core.AppBar("ABE_TOP", 0) -Property @{
+	ClientSize = New-Object System.Drawing.Size(1940, 70)
+	FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::None
+	Name = "AppBar"
+	StartPosition = [System.Windows.Forms.FormStartPosition]::Manual
+}
+
+[System.Windows.Forms.Button]$StartButton = New-Object System.Windows.Forms.Button -Property @{
+	Location = New-Object System.Drawing.Point(10, 10)
+	Name = "StartButton"
+	Text = "Start"
+	Size = New-Object System.Drawing.Size(50, 50)
+	TabIndex = 1
+}
+
+[System.Windows.Forms.Panel]$OpenWindows = New-Object System.Windows.Forms.Panel -Property @{
+	Anchor = 15 #All Edges
+	Location = New-Object System.Drawing.Point(70, 10)
+	Name = "RunningApps"
+	Size = New-Object System.Drawing.Size(834, 50)
+}
+
+#TODO: Create DateLabel, group them in panel, center them.
+[System.Windows.Forms.Label]$TimeLabel = New-Object System.Windows.Forms.Label -Property @{
+	Text = [System.DateTime]::Now.ToString("HH:mm") + "
+	" + [System.DateTime]::Today.ToString("dd.MM.yyyy")
+	Size = New-Object System.Drawing.Size(95, 50)
+	Location = New-Object System.Drawing.Point(($Bar.width - 105), 10)
+	Anchor = 9 #Top Right
+	Font = New-Object System.Drawing.Font("Microsoft Sans Serif", 11.25, [System.Drawing.FontStyle]::Bold, [System.Drawing.GraphicsUnit]::Point, [Byte]0)
+	TextAlign = [System.Drawing.ContentAlignment]::MiddleCenter
+}
+
+$Bar.SuspendLayout()
+$Bar.Controls.AddRange(($StartButton, $OpenWindows, $TimeLabel))
+$Bar.ResumeLayout($False)
+$Bar.PerformLayout()
 
 . '.\startMenu.ps1'
-$startM = startMenu($Bar)
+[System.Windows.Forms.Form]$startM = startMenu($Bar)
 
-$Bar.startButton.Add_Click(
+$StartButton.Add_Click(
     {
         If (-not ($startM.Visible)) {
             $startM.Show()
@@ -31,16 +67,28 @@ $Bar.startButton.Add_Click(
 }
 $myTime.Add_Tick(
 	{
-		$Bar.ticked($sender, $eventArgs)
-		$Bar.TimeLabel.Text = [System.DateTime]::Now.ToString("HH:mm")
+		#$Bar.ticked($sender, $eventArgs)
+		$TimeLabel.Text = [System.DateTime]::Now.ToString("HH:mm") + "
+	" + [System.DateTime]::Today.ToString("dd.MM.yyyy")
+		$intLeft = 0
+		$OpenWindows.SuspendLayout()
+		$OpenWindows.Controls.Clear()
+		
+		
+		foreach($Win in (Get-Process | where-object {$_.MainWindowHandle -ne 0 -And $_.MainWindowTitle -ne ""} | Select-Object mainWindowTitle, mainwindowhandle | Sort-Object -Property MainWindowHandle))
+		{
+			#Write-Host ($Bar.EnumCallBack($Win.MainWindowHandle, $Win.MainWindowTitle))
+			$d = New-Object Shell4Core.AppBarIcon $Win.MainWindowHandle
+			$d.Text = $Win.MainWindowTitle
+			$d.ArrangeMe([ref]$intLeft)
+			$OpenWindows.Controls.Add($d)
+		}
+		$OpenWindows.ResumeLayout()
 	})
-#Solve the pipeline issue on close
-$myTime.Interval = 2000
-$myTime.Enabled = $True
+
 
 $Bar.Add_Load(
 	{
-		$Bar.StartPosition = [System.Windows.Forms.FormStartPosition]::Manual
 		write-host "Loading AppBar.."
 		$Bar.RegisterBar()
 	})
@@ -49,3 +97,4 @@ $Bar.Add_Load(
 
 #Later...
 #http://www.codeproject.com/Tips/895840/Multi-Threaded-PowerShell-Cookbook
+#Solve the pipeline issue on close <= https://social.technet.microsoft.com/Forums/office/en-US/573c1870-5e31-43a1-a863-3f2ebed418df/how-to-handle-close-event-of-powershell-window-if-user-clicks-on-closex-button?forum=winserverpowershell
